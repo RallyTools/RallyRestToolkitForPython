84e5215eb23d7e3bf3d1407ce0423508436f6c38
Kip Lehman <klehman@rallydev.com>
2012-03-27 22:20:15 -0600

    upped rev info in pyral modules to (0,8,12)

--- pyral/__init__.py
+++ pyral/__init__.py
@@ -1,4 +1,4 @@
-__version__ = (0, 8, 11)
+__version__ = (0, 8, 12)
 from .config    import rallySettings
 from .restapi   import Rally, RallyRESTAPIError, RallyUrlBuilder
 from .rallyresp import RallyRESTResponse
