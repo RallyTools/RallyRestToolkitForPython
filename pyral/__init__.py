0a6563d8e19f9b5c9f08f8aa06191db9b5d8b180
Kip Lehman <klehman@rallydev.com>
2017-08-22 10:30:30 -0600

    upped __init__.py version to 1.4.0 as first action to support Connection/PullRequest entities

--- pyral/__init__.py
+++ pyral/__init__.py
@@ -1,4 +1,4 @@
-__version__ = (1, 3, 2)
+__version__ = (1, 4, 0)
 from .config    import rallySettings, rallyWorkset
 from .restapi   import Rally, RallyRESTAPIError, RallyUrlBuilder
 from .restapi   import AgileCentral, AgileCentralRESTAPIError, AgileCentralUrlBuilder
